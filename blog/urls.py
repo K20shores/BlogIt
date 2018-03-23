4df69bab2a7d08d42cd4d7ebf50e410bd8aac8e9
Kyle Shores <kyle.shores44@gmail.com>
2018-03-23 15:58:09 -0500

    Can create, edit, and delte all blogs
    Blogs correctly only let you delete or edit blogs
    that belong to that user.

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create', views.create, name='create'),
+    path('save_blog', views.save_blog, name='save_blog'),
+    path('edit/<int:blog_id>', views.edit, name='edit'),
+    path('delete/<int:blog_id>', views.delete, name='delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 
 from blog.models import BlogPost
@@ -6,12 +6,32 @@ from blog.models import BlogPost
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/index.html', {})
+    return render(request, 'blog/index.html', {'blog' : None})
 
-def create(request):
+def save_blog(request):
     blog_title = request.POST.get('blog_title', None)
     blog_text = request.POST.get('blog_text', None)
-    blog_post = BlogPost(blog_title=blog_title, blog_text=blog_text, pub_date=timezone.now(), blog_author=request.user)
-    print(blog_post)
-    blog_post.save()
+    blog_id = request.POST.get('blog_id', None)
+    print("blog id: {0}\nblog text: {1}".format(blog_id, blog_text))
+    if blog_id is not None:
+        blog_post = get_object_or_404(BlogPost, pk=blog_id)
+        blog_post.blog_title = blog_title
+        blog_post.blog_text = blog_text
+        blog_post.pub_date = timezone.now()
+        print(blog_post)
+        blog_post.save()
+    else:
+        blog_post = BlogPost(blog_title=blog_title, blog_text=blog_text, pub_date=timezone.now(), blog_author=request.user)
+        print(blog_post)
+        blog_post.save()
     return redirect('/')
+
+def edit(request, blog_id):
+    blog = get_object_or_404(BlogPost, pk=blog_id)
+    return render(request, 'blog/index.html', {'blog' : blog})
+
+def delete(request, blog_id):
+    blog = get_object_or_404(BlogPost, pk=blog_id)
+    blog.delete()
+    return redirect('/')
+
